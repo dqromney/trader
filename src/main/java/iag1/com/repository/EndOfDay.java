4ca75b7627edbf4fcc22d3e193c022d278ea4d70
David Quenten Romney <david.romney@ldschurch.org>
2015-09-30 10:30:00 -0600

    Added information in AppConfig.properties about other Yahoo API calls. Changed name of original URL.

--- src/main/java/iag1/com/repository/EndOfDay.java
+++ src/main/java/iag1/com/repository/EndOfDay.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.text.ParseException;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -26,9 +27,19 @@ public class EndOfDay {
      * @throws ParseException
      */
     public List<Bar> all(final String pSymbol) throws IOException, ParseException {
-        URL url = new URL(AppConfig.YAHOO_EOD_WEB_SERVICE_URL.getValue() + pSymbol);
+        URL url = new URL(AppConfig.YAHOO_CHART_WEB_SERVICE_URL.getValue() + pSymbol);
         FileLoader fileLoader = new FileLoader(new BufferedReader(new InputStreamReader(url.openStream())));
         return fileLoader.readBars();
     }
 
+    /**
+     * Retrieve history for the last number of days.
+     *
+     * @param pSymbol the {@link String} symbol
+     * @param pDays the number {@link Integer} of days
+     * @return a list of end-of-day data for the symbol
+     */
+    public List<Bar> historyForDays(final String pSymbol, final Integer pDays) {
+        return Collections.EMPTY_LIST;
+    }
 }
--- src/main/java/iag1/com/types/AppConfig.java
+++ src/main/java/iag1/com/types/AppConfig.java
@@ -2,11 +2,20 @@ package iag1.com.types;
 
 /**
  * Application Configuration.
+ * Note(s):
+ * Some other JSON based real-time quotes - used for alerting, SMS, etc.
+ * See http://stackoverflow.com/questions/27543776/yahoo-finance-webservice-api
+ * http://finance.yahoo.com/webservice/v1/symbols/COALINDIA.NS/quote?format=json
+ * http://finance.yahoo.com/webservice/v1/symbols/COALINDIA.NS/quote?format=json&view=detail
+ * http://finance.yahoo.com/webservice/v1/symbols/YHOO,AAPL/quote?format=json&view=detail
+ * 15 minute delayed.
+ * http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22YHOO%22,%22AAPL%22)&format=json&env=http://datatables.org/alltables.env
  *
  * Created by dqromney on 9/26/15.
  */
 public enum AppConfig {
-    YAHOO_EOD_WEB_SERVICE_URL("http://ichart.yahoo.com/table.csv?s="),
+    YAHOO_CHART_WEB_SERVICE_URL("http://ichart.yahoo.com/table.csv?s="),
+    YAHOO_FINANCE_WEB_SERVICE_URL("http://finance.yahoo.com/webservice/v1/symbols/"),
     YAHOO_EOD_HEADER_DATE("Date"),
     YAHOO_EOD_HEADER_OPEN("Open"),
     YAHOO_EOD_HEADER_HIGH("High"),
